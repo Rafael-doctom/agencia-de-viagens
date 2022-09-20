a8842e3c548496b42ce8fd0fd06110985c92d3e6
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-09-20 13:46:20 -0300

    started application

---
+++ projects/src/components/header/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+const Header = () => {
+    return(
+        <>
+        <div className="header">
+        header
+        </div>
+        </>
+    );
+};
+
+export default Header;
